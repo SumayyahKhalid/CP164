a224a4f4c0f0adcf99ece2f3b1ad91c7a7ecb57b
SumayyahKhalid <152665748+SumayyahKhalid@users.noreply.github.com>
2024-01-27 17:22:05 -0500

    Add files via upload

---
+++ L01/src/t02.py
@@ -0,0 +1,12 @@
+'''
+Author: Sumayyah Khalid
+ID: 169022335
+Email: khal2335@mylaurier.ca
+__updated__:Jan. 11, 2024
+'''
+
+from Food import Food
+
+p = Food("Ravioli", 7 , False, 246)
+
+print(p.__str__())
